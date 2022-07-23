6be8b5683aa40c239bbff0704501ba22914084f9
Avinash <avinashmohan2001@gmail.com>
2022-07-23 23:13:53 +0530

    Day 9

---
+++ Day 09 - Search in a Binary Search Tree.java
@@ -0,0 +1,36 @@
+// Leetcode Problem Link: https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if(root == null)
+            return root;
+        
+        if(root.val == val)
+            return root;
+        
+        else if(root.val > val)
+            return searchBST(root.left, val);
+        
+        else if(root.val < val)
+            return searchBST(root.right, val);
+        
+        return null;
+        
+    }
+}
