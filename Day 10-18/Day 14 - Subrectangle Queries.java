43c858d033c69b01967d3043e34c11ae15aa7703
Avinash <avinashmohan2001@gmail.com>
2022-07-28 21:38:12 +0530

    Day 14

---
+++ Day 10-18/Day 14 - Subrectangle Queries.java
@@ -0,0 +1,29 @@
+// Leetcode Problem Link: https://leetcode.com/problems/subrectangle-queries/
+
+class SubrectangleQueries {
+
+    int[][] rectangle;
+    
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+    
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for(int row = row1; row <= row2; row++){
+            for(int col = col1; col <= col2; col++){
+                rectangle[row][col] = newValue;
+            }
+        }
+    }
+    
+    public int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj.getValue(row,col);
+ */
