314adb9511c181e8ce014c7cb76d1b08c3ceb87a
Avinash <avinashmohan2001@gmail.com>
2022-07-28 16:15:17 +0530

    Day 14 = Brain Kernighan's Algorithm

---
+++ Day 10-18/Day 14 - Number of 1 Bits -  Brian Kernighan’s Algorithm.java
@@ -0,0 +1,20 @@
+// Leetcode Problem Link: https://leetcode.com/problems/number-of-1-bits/
+
+// Brian Kernighan’s Algorithm: 
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        int countOfOnes = 0;
+        
+        while(n != 0){
+            
+            n = n&(n-1);
+            countOfOnes++;
+            
+        }
+        
+        return countOfOnes;
+        
+    }
+}
