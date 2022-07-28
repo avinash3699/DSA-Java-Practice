99f16d6c6f9dfa44cd66bae1c66dafa7e453db69
Avinash <avinashmohan2001@gmail.com>
2022-07-28 15:33:38 +0530

    Day 14

---
+++ Day 10-18/Day 14 - Reverse String.java
@@ -0,0 +1,18 @@
+// Leetcode Problem Link: https://leetcode.com/problems/reverse-string/
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int i = 0, j = s.length - 1;
+        while(i < j){
+            
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            
+            i++;
+            j--;
+        }
+        
+    }
+}
