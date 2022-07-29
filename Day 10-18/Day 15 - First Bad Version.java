cc24466c942f0bbe6130918e81eb4a7076fd7861
Avinash <avinashmohan2001@gmail.com>
2022-07-29 15:06:49 +0530

    Day 15

---
+++ Day 10-18/Day 15 - First Bad Version.java
@@ -0,0 +1,28 @@
+// Leetcode Problem Link: https://leetcode.com/problems/first-bad-version/
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int firstBadVersion = 1;
+        int low = 0, high = n;
+        
+        while(low <= high){
+            
+            int mid = low + ((high - low) / 2);
+            
+            if(isBadVersion(mid)){
+                firstBadVersion = mid;
+                high = mid - 1;
+            }
+            
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return firstBadVersion;
+    }
+}
