c58d63d24695a96378480aa1ebbaf2beff871e95
Avinash <avinashmohan2001@gmail.com>
2022-08-09 21:31:01 +0530

    Day 26

---
+++ Day 19-27/Day 26 - Stable Selection Sort.java
@@ -0,0 +1,39 @@
+// Leetcode Program Link: https://leetcode.com/problems/sort-an-array/
+
+class Solution {
+    public int[] sortArray(int[] array) {
+            
+        selectionSort(array);
+        return array;
+        
+    }
+    
+    public static void selectionSort(int[] arr){
+        
+        int arrLength = arr.length;
+        
+        for(int ind1 = 0; ind1 < arrLength-1; ind1++){
+            
+            int minIndex = ind1;
+            
+            for(int ind2 = ind1+1; ind2 < arrLength; ind2++){
+                
+                if(arr[ind2] < arr[minIndex])
+                    minIndex = ind2;
+                
+            }
+
+            int value = arr[minIndex];
+            
+            while(minIndex > ind1){
+                arr[minIndex] = arr[minIndex - 1];
+                minIndex--;
+            }
+            
+            arr[ind1] = value;
+            
+        }
+        
+    }
+    
+}
